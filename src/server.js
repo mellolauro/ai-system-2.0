ece6e8e06332aa9c4ddfc251814e099718ef38ab
lauromello <lauromello@hotmail.com>
2026-09-20 20:39:28 -0300

    feat: add HTTP security headers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const helmet = require("helmet");
 const session = require("express-session");
 const { Pool } = require("pg");
 const pgSession = require("connect-pg-simple")(session);
@@ -55,6 +56,22 @@ app.use(express.json());
 app.use(
     express.urlencoded({
         extended: true
+    })
+);
+
+/*
+ * ============================================================
+ * SECURITY HEADERS
+ * ============================================================
+ */
+
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+
+        referrerPolicy: {
+            policy: "strict-origin-when-cross-origin"
+        }
     })
 );
 
